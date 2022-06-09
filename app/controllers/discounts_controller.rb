dfbc5decd5b07492f393c52b72ea2c6904e043e3
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-09 19:09:04 -0400

    Add functionality for editing discount

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,5 +1,5 @@
 class DiscountsController < ApplicationController
-  before_action :find_merchant, only: [:index, :show, :new, :create, :destroy]
+  before_action :find_merchant, only: [:index, :show, :new, :create, :destroy, :edit, :update]
 
   def index
   end
@@ -23,11 +23,21 @@ class DiscountsController < ApplicationController
     redirect_to merchant_discounts_path(@merchant)
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+  
+    discount = Discount.find(params[:id])
+    discount.update(discount_params)
+    redirect_to merchant_discount_path(@merchant, discount)
+  end
+
 
 private
 
-def discount_params
-  params.require(:discount).permit(:percentage, :threshold)
-
-end
+  def discount_params
+    params.require(:discount).permit(:percentage, :threshold)
+  end
 end
