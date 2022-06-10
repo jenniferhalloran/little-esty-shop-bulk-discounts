aed88ae14a51e25049cf9ece83dfb8bad931c64f
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-10 10:25:30 -0400

    Update relationships

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,7 +1,9 @@
 class Discount < ApplicationRecord
   validates_presence_of :percentage, :threshold
-  validates_numericality_of :percentage, :threshold
+  validates_numericality_of :percentage, {greater_than_or_equal_to: 1, less_than_or_equal_to: 99}
+  validates_numericality_of :threshold, greater_than_or_equal_to: 1
 
   belongs_to :merchant
+  has_many :invoices, through: :merchant
 
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,10 +1,19 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+  has_many :discounts, through: :item
+  has_many :merchants, through: :item
+
 
   validates :quantity, presence: true
   validates :unit_price, presence: true
   validates :status, presence: true
 
   enum status: {'pending' => 0, 'shipped' => 1, 'packaged' => 2}
+
+
+  # def applied_discount
+  #   require "pry"; binding.pry
+  #
+  # end
 end
