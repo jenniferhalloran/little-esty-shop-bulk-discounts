1b4a591fb99acace7d9a6d70b3fb2b594f3838a5
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-14 12:58:37 -0400

    Add test for invalid input when creating a discount

--- spec/features/merchants/discounts/new_spec.rb
+++ spec/features/merchants/discounts/new_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'Merchant Discount new page' do
   before do
     @rei = Merchant.create!(name: "REI")
-    visit new_merchant_discount_path(@rei)
   end
 
   it 'allows the user to create a discount', :vcr do
@@ -19,6 +18,17 @@ describe 'Merchant Discount new page' do
 
     expect(current_path).to eq(merchant_discounts_path(@rei))
     expect(page).to have_content("25% off 5 of the same item")
+  end
+
+  it "displays an error message if user tried to make a discount under 1% or over 99%" do
+    visit new_merchant_discount_path(@rei)
+
+    fill_in 'discount_percentage', with: 100
+    fill_in 'discount_threshold', with: 5
+    click_button("Create Discount")
+
+    expect(current_path).to eq(new_merchant_discount_path(@rei))
+    expect(page).to have_content("Please enter a valid discount between 1 - 99%")
 
   end
 end
